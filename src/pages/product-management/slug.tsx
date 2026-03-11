80c03bbf3cc31c3f16109a295e622fa16062c4db
Quynh Do <doducquynh14022000@gmail.com>
2026-03-11 23:23:47 +0700

    slug

--- src/pages/product-management/slug.tsx
+++ src/pages/product-management/slug.tsx
@@ -272,7 +272,7 @@ const SlugProductManagement = () => {
                                     <InputMultiCommon
                                         label={"Từ khóa"}
                                         attribute={"keyword"}
-                                        isRequired={true}
+                                        isRequired={false}
                                         dataAttribute={dataRequest.keyword}
                                         setData={setDataRequest}
                                         disabled={false}
